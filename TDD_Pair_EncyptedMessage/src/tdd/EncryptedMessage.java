30ba72bea2badcef5217c2d4a90bc185a9359e00
SkrochSimon <SkrochSimon@10.100.8.242>
2023-11-07 15:12:30 -0600

    Fixed message and key arrays

--- TDD_Pair_EncyptedMessage/src/tdd/EncryptedMessage.java
+++ TDD_Pair_EncyptedMessage/src/tdd/EncryptedMessage.java
@@ -11,14 +11,18 @@ import java.util.ArrayList;
  */
 public class EncryptedMessage {
 
-	private String mEncryptedMessage;
+	private String mEncryptedMessage = "";
 	/**
 	 * @throws Exception 
 	 * 
 	 */
 	public EncryptedMessage(String message, String key) {
 		// TODO Auto-generated constructor stub
-		encryptMessage(message, key);
+	     if(validate(message) && validate(key)) {
+		    encryptMessage(message, key);
+	     } else {
+	       throw new Exception("Unauthorized use.")
+	     }
 	}
 	
 	public EncryptedMessage(String encryptedMessage) {
@@ -36,7 +40,17 @@ public class EncryptedMessage {
 	}
 	
 	private Boolean validate(String data) {
-		return false;
+		boolean valid = false;
+		int invalid = 0;
+	  for(char c: data) {
+		  if(c < 'A' || c > 'Z') {
+		    invalid++;
+		  }
+		}
+	  if(invalid == 0) {
+	    valid = true;
+	  }
+	  return valid;
 	}
 
 	/**
@@ -49,22 +63,19 @@ public class EncryptedMessage {
 		//set to upper (case is lost)
 	  if (message != null) {	    
 	    message = message.toUpperCase();
+	    key = key.toUpperCase();
 	    //break message and key into arraylists of characters
 	    ArrayList<Character> messageChars = new ArrayList<Character>();
 	    ArrayList<Character> keyChars = new ArrayList<Character>();
 	    
-	    //remove number and special characters from message
-	    for (int i = 0; i < messageChars.size(); i++) {
-	      if ((int)messageChars.get(i) < 65 || (int)messageChars.get(i) > 90) {
-	        messageChars.remove(i);
-	      }
+	    //fill messageChars from message
+	    for (char c : message) {
+	      messageChars.add(c);
 	    }
 	    
-	    //remove number and special characters from message
-	    for (int i = 0; i < keyChars.size(); i++) {
-	      if ((int)keyChars.get(i) < 65 || (int)keyChars.get(i) > 90) {
-	        keyChars.remove(i);
-	      }
+	    //fill keyChars from key
+	    for (char c : key) {
+	      keyChars.add(c);
 	    }
 	    
 	    //extend key to match message length if shorter than message
@@ -79,9 +90,9 @@ public class EncryptedMessage {
 	    //loop through the key and message one by one
 	    for (int i = 0; i < messageChars.size(); i++) {
 	      //update each letter in the message by adding the ascii value of char from letter in message plus difference of letter from key minus 65 (A)
-	      int temp = messageChars.get(i);
-	      temp += ((int)keyChars.get(i) - 65);
-	      messageChars.set(i, (char)temp);
+	     
+	      messageChars.set(i, (messageChar.get(i) + keyChars.get(i) - 'A') );
+	      System.out.print(messageChars.get(i));
 	    }
 	    
 	    //condense encrypted message to string and return
